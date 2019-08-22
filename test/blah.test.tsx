438f99094d075b698ec1f97108cf3145e4b579a1
kresooo <kresimir.pehar@pubnub.com>
2019-08-22 10:29:29 +0200

    add

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,5 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+const value = 4;
 
-describe('it', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+test('number', () => {
+  expect(value).toBe(4);
 });
